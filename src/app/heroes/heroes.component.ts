a41d493382e94a6a2965d0f0a57eea1ec3c0325c
Javiceta <javigarciaiceta@gmail.com>
2019-02-07 10:27:42 +0000

    add hero as a component with properties

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
 
 //metadata
 @Component({
@@ -8,7 +9,10 @@ import { Component, OnInit } from "@angular/core";
 })
 //always EXPORT class so the component can be IMPORTED later
 export class HeroesComponent implements OnInit {
-  hero = "Soldier 76";
+  hero: Hero = {
+    id: 1,
+    name: "Soldier 76"
+  };
   constructor() {}
 
   //called on create. Good for initialization logic
