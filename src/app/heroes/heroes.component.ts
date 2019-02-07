aff0008fd761138484be8f6179b75e78e2eb87e8
Javiceta <javigarciaiceta@gmail.com>
2019-02-07 10:21:16 +0000

    add and show name property of a hero component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
+//metadata
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
+//always EXPORT class so the component can be IMPORTED later
 export class HeroesComponent implements OnInit {
+  hero = "Soldier 76";
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  //called on create. Good for initialization logic
+  ngOnInit() {}
 }
